9d0bd697363fbe8c807e726235422e95fa71fc30
samithaharshana <shpalihawadana@gmail.com>
2017-02-23 18:18:28 +0530

    demo 6.8.1

--- src/main/java/com/demo/Launcher.java
+++ src/main/java/com/demo/Launcher.java
@@ -20,11 +20,11 @@ public class Launcher {
 		System.out.println("3rd change");
 //	git commit -m
 		
-		System.out.println("3rd change");
-		System.out.println("3rd change");
-		System.out.println("3rd change");
-		System.out.println("3rd change");
-		
+//		System.out.println("3rd change");
+//		System.out.println("3rd change");
+//		System.out.println("3rd change");
+//		System.out.println("3rd change");
+//
 		System.out.println("3rd change");
 		System.out.println("3rd change");
 		System.out.println("3rd change");
